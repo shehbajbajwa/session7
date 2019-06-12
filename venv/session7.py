72be1923910c72ea10b933134a51846ae00bf474
shehbaj bajwa <bajwashehbaj@gmail.com>
2019-06-12 10:40:06 +0530

    Initial commit

---
+++ venv/session7.py
@@ -0,0 +1,9 @@
+num = 10
+def show():
+    global num
+    num = num % 3
+    print(">>1. num is:",num)
+show()
+print(">>2. num is:",num)
+
+age = 20
---
+++ venv/session7A.py
@@ -0,0 +1,25 @@
+"""
+object oriented programming structure
+oops
+> object
+> class
+         1. encapsulation
+         2. abstraction
+          3 inheritance
+          4 polymorphism
+
+          real world
+          object is anything u can feel or touch, which is a real
+
+          class is drawing of an object
+          class is representation how an object will look like
+
+          what will you think about first????
+          1. object
+          2. draw object
+          3 create real object by looking at drawing
+      cse
+         object : multivalue container
+         if we wish to customise multivalue container we will create object
+         class : textual representation of object
+"""
